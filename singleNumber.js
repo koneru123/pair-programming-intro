f0f3eb8333d1abe7990dc50f93a480d7255c14c4
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-14 11:31:49 -0800

    singleNumber function

--- singleNumber.js
+++ singleNumber.js
@@ -8,6 +8,44 @@ Desired output:
 1
 */
 
-//Navigator: ENTER_NAME
-//Driver: ENTER_NAME
+//Navigator: Priyanka Koneru
+//Driver: Matthew Guidone
 //code here
+
+// var arrayOfIntegers = [1, 1, 2, 2, 3, 4, 4];
+
+/*
+
+Steps
+- Create value variable for output
+- Iterate through array, ignore duplicates
+- Return output
+
+*/
+
+var arrayOfIntegers = [1,2,2,4,4];
+//{'1' : 2, '2':2, '3' : 1, '4': 2}
+
+let test = function(arr) {
+  let obj = {};
+  let output = 0;
+  for(let i = 0; i < arrayOfIntegers.length; i++) {
+    if(!obj[arrayOfIntegers[i]]) {
+      obj[arrayOfIntegers[i]] = 1;
+    } else {
+      obj[arrayOfIntegers[i]]++;
+    }
+  }
+
+  for(let j in obj) {
+    if(obj[j] < 2) {
+      output = j;
+    }
+  }
+  return output;
+}
+
+console.log(test(arrayOfIntegers));
+
+
+
