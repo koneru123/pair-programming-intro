5ce2add96b2c7bf56d08a643b365fd80cffa73d3
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-14 17:09:07 -0800

    Solved keyboardRow function

--- keyboardRow.js
+++ keyboardRow.js
@@ -1,18 +1,25 @@
-/*
-Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below.
+const test = function keyboard(arr) {
+  var letters = ['a', 'A', 's', 'S', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H', 'j', 'J', 'k', 'K', 'l', 'L'];
 
+  let newFilteredArr = [];
 
-American keyboard
+  for(let i = 0; i < arr.length; i++) {
+    let eachWord = arr[i];
+    let wordExists = true;
+    for(let j = 0; j < eachWord.length; j++) {
+      let eachWordLetter = eachWord[j];
+      if(!letters.includes(eachWordLetter)) {
+        wordExists = false;
+        break;
+      }
+    }
+    if(wordExists) {
+      newFilteredArr.push(eachWord);
+    }
+  }
+  return JSON.stringify(newFilteredArr);
+}
 
+var input = ["Hello", "Alaska", "Dad", "Peace"];
 
-Example 1:
-Input: ["Hello", "Alaska", "Dad", "Peace"]
-Output: ["Alaska", "Dad"]
-Note:
-You may use one character in the keyboard more than once.
-You may assume the input string will only contain letters of alphabet.
-*/
-
-// Navigator: ENTER_NAME
-// Driver: ENTER_NAME
-// code here
+console.log(test(input));
